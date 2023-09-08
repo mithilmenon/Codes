8f0b3a0409ed35c918da6c8c96ad1a4d29920902
Mithil Menon <mithilmenon11@gmail.com>
2023-09-08 15:45:49 +0530

    File Reading using Threads

---
+++ src/ExecutorServiceFiles.java
@@ -0,0 +1,7 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceFiles {
+    ExecutorService executorService= Executors.newFixedThreadPool(2);
+    executorService.execute=
+}
